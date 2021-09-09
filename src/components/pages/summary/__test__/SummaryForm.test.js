39fa04d458e2d4b526a616431016cf94e1f06e5e
aniket <aniket.umbc@gmail.com>
2021-09-09 14:50:10 +0530

    Test and code summary form

---
+++ src/components/pages/summary/__test__/SummaryForm.test.js
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import SummaryFrom from '../SummaryForm';
+
+it('Should render checkbox correctly', () => {
+  render(<SummaryFrom />);
+  const checkBoxEle = screen.getByRole('checkbox', {
+    name: 'I agree to Terms and Conditions',
+  });
+
+  expect(checkBoxEle).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: 'Confirm Order' });
+  expect(confirmButton).toBeDisabled();
+});
+
+it('Should enable button on first click and disables on second click', () => {
+  render(<SummaryFrom />);
+  const checkBoxEle = screen.getByRole('checkbox', {
+    name: 'I agree to Terms and Conditions',
+  });
+
+  const confirmButton = screen.getByRole('button', { name: 'Confirm Order' });
+
+  fireEvent.click(checkBoxEle);
+
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkBoxEle);
+
+  expect(confirmButton).toBeDisabled();
+});
