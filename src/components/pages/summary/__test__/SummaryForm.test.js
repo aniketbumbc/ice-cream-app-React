9393bc8240203a9af29fa53ab2e15f9f1156449a
aniket <aniket.umbc@gmail.com>
2021-09-22 11:18:09 +0530

    Mock service worker setup

--- src/components/pages/summary/__test__/SummaryForm.test.js
+++ src/components/pages/summary/__test__/SummaryForm.test.js
@@ -1,5 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import SummaryFrom from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 it('Should render checkbox correctly', () => {
   render(<SummaryFrom />);
@@ -21,11 +26,36 @@ it('Should enable button on first click and disable on second click checkbox', (
 
   const confirmButton = screen.getByRole('button', { name: 'Confirm Order' });
 
-  fireEvent.click(checkBoxEle);
+  userEvent.click(checkBoxEle);
 
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkBoxEle);
+  userEvent.click(checkBoxEle);
 
   expect(confirmButton).toBeDisabled();
+});
+
+it('popover responds to hover', async () => {
+  render(<SummaryFrom />);
+  // start out hidden
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actully be delivered/i
+  );
+  expect(nullPopover).toBeNull();
+
+  //mouse over on label
+
+  const termsAndConditions = screen.getByText(/Terms and Conditions/);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/No icecream will actually delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // disappears when mouse out
+  userEvent.unhover(termsAndConditions);
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/No icecream will actually delivered/i)
+  );
 });
---
+++ src/mock/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+    return res(
+      ctx.res([
+        {
+          name: 'Mint chip',
+          imagePath: '/images/mint-chip.png',
+        },
+        {
+          name: 'Vanilla',
+          imagePath: '/images/vanilla.png',
+        },
+        {
+          name: 'Chocolate',
+          imagePath: '/images/chocolate.png',
+        },
+      ])
+    );
+  }),
+];
