bfc31743536544b8d5447d4a0885fec871d90112
aniket <aniket.umbc@gmail.com>
2021-09-27 15:31:10 +0530

    topping totals added in test cases

--- src/components/pages/entry/__test__/totalUpdates.test.js
+++ src/components/pages/entry/__test__/totalUpdates.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
 import { OrderDetailsProvider } from '../../../../context/OrderDetails';
 
 it('update scoop subtotal when change', async () => {
-  render(<Options optionType='scoops' />, { wrapper: OrderDetailsProvider });
+  render(<Options optionType='scoops' />);
   // make sure subtotal 0
   const scoopTotal = screen.getByText('Scoops total: $', { exact: false });
 
@@ -29,4 +29,28 @@ it('update scoop subtotal when change', async () => {
   userEvent.type(choclateInput, '2');
 
   expect(scoopTotal).toHaveTextContent('6.00');
+});
+
+it('update topping subtotal when toppings change', async () => {
+  render(<Options optionType='toppings' />);
+
+  const toppingsTotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  });
+  expect(toppingsTotal).toHaveTextContent('0.00');
+
+  const gummieBearsCheckbox = await screen.findByRole('checkbox', {
+    name: 'Gummi bears',
+  });
+  userEvent.click(gummieBearsCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
+
+  const mochiCheckbox = await screen.findByRole('checkbox', {
+    name: 'Mochi',
+  });
+  userEvent.click(mochiCheckbox);
+  expect(toppingsTotal).toHaveTextContent('3.00');
+
+  userEvent.click(mochiCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
 });
