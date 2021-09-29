20b1a5f20a9cdc753e34a9d1d0671c8f75c8c6da
aniket <aniket.umbc@gmail.com>
2021-09-29 21:20:00 +0530

    solve error in test case unmount component

--- src/components/pages/entry/OrderEntry.jsx
+++ src/components/pages/entry/OrderEntry.jsx
@@ -1,10 +1,14 @@
 import Options from './Options';
+import { useOrderDetails } from '../../../context/OrderDetails';
 
 export default function OrderEntry() {
+  const [orderDetails] = useOrderDetails();
   return (
     <div>
+      <h1>Design Your Sundae!!</h1>
       <Options optionType='scoops' />
       <Options optionType='toppings' />
+      <h2>Grand total: {orderDetails.total.grandTotal}</h2>
     </div>
   );
 }
--- src/components/pages/entry/__test__/totalUpdates.test.js
+++ src/components/pages/entry/__test__/totalUpdates.test.js
@@ -1,6 +1,13 @@
-import { render, screen } from '../../../../test-utils/testing-library-utils';
+import {
+  render,
+  screen,
+  getByText,
+  findByRole,
+  getByRole,
+} from '../../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import OrderEntry from '../OrderEntry';
 
 it('update scoop subtotal when change', async () => {
   render(<Options optionType='scoops' />);
@@ -52,4 +59,70 @@ it('update topping subtotal when toppings change', async () => {
 
   userEvent.click(mochiCheckbox);
   expect(toppingsTotal).toHaveTextContent('1.50');
+});
+
+describe('grand total', () => {
+  it('Should updated grand total properly if scoop is added first', async () => {
+    render(<OrderEntry />);
+
+    const grandTotalEle = screen.getByRole('heading', {
+      name: /Grand Total: \$/i,
+    });
+
+    expect(grandTotalEle).toHaveTextContent('$0.00');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, '1');
+
+    expect(grandTotalEle).toHaveTextContent('2.00');
+
+    const choclateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    userEvent.clear(choclateInput);
+    userEvent.type(choclateInput, '1');
+    expect(grandTotalEle).toHaveTextContent('4.00');
+  });
+
+  it('Should updated grand total properly if toopings is added first', async () => {
+    render(<OrderEntry />);
+
+    const grandTotalEle = screen.getByRole('heading', {
+      name: /Grand Total: \$/i,
+    });
+    const gummieBearsCheckbox = await screen.findByRole('checkbox', {
+      name: 'Gummi bears',
+    });
+    userEvent.click(gummieBearsCheckbox);
+    expect(grandTotalEle).toHaveTextContent('1.50');
+  });
+
+  it('Should updated grand total properly if removed item', async () => {
+    render(<OrderEntry />);
+    const grandTotalEle = screen.getByRole('heading', {
+      name: /Grand Total: \$/i,
+    });
+
+    const gummieBearsCheckbox = await screen.findByRole('checkbox', {
+      name: 'Gummi bears',
+    });
+    userEvent.click(gummieBearsCheckbox);
+    expect(grandTotalEle).toHaveTextContent('1.50');
+
+    const choclateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    userEvent.clear(choclateInput);
+    userEvent.type(choclateInput, '1');
+    expect(grandTotalEle).toHaveTextContent('3.50');
+
+    userEvent.click(gummieBearsCheckbox);
+    expect(grandTotalEle).toHaveTextContent('2.00');
+  });
 });
